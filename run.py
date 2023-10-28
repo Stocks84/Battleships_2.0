6ecbfb888098a0fc136f415dbdae219081aa6287
William Stocks <willstocks@me.com>
2023-10-28 18:15:36 +0000

    Update game_board function

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
-def game_board():
+def game_board(hit,miss,elim):
     """
     Game_board creates the board.
     """
-    print("Battleships")
-    print(" 0 1 2 3 4 5 6 7")
+    print("       Battleships\n")
+    print("     0  1  2  3  4  5  6  7")
 
     place = 0
     for x in range(8):
@@ -21,3 +21,10 @@ def game_board():
             row = row + ch
             place = place + 1
         print(x," ",row)
+
+
+hit = [21,22]
+miss = [20,24,12,13]
+elim = [23]
+
+game_board(hit,miss,elim)
